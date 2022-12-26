b36c79cc9ba00b79611b030fe28267ae93677464
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-12-26 23:56:04 +0900

    Auth.jsx(firebase config) 파일 생성

---
+++ src/Auth.jsx
@@ -0,0 +1,17 @@
+// 2022 06 21 기준 firebase import
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUXNkK2ELz5E2__WQMYnH7SAz1oxI7k30",
+  authDomain: "search-engine-c6d9b.firebaseapp.com",
+  projectId: "search-engine-c6d9b",
+  storageBucket: "search-engine-c6d9b.appspot.com",
+  messagingSenderId: "1046304289765",
+  appId: "1:1046304289765:web:23070e0acb046ca81df50c",
+  measurementId: "G-46E2FNRZTX",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
