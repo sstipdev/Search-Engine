532246e1a1c3f043d3d60fa68970cd4532abe10b
hmyo2853 <hmyo2853@gmail.com>
2022-12-26 20:16:09 +0900

    fetch data 수정

--- src/components/ChoiceType.jsx
+++ src/components/ChoiceType.jsx
@@ -1,21 +1,31 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Logo from "../assets/logo.jpeg";
 import WebResult from "./WebResult";
 import VideoResult from "./VideoResult";
 import ImageResult from "./ImageResult";
 import BlogResult from "./BlogResult";
 
-import { ChoiceTypeBox, MainName, LogoImg, Header, UserForm, UserInput, SubmitBtn, ChoiceBox, SearchType, MainResult } from "../styles/ChoiceTypeCSS";
+import {
+  ChoiceTypeBox,
+  MainName,
+  LogoImg,
+  Header,
+  UserInput,
+  ChoiceBox,
+  SearchType,
+  MainResult,
+} from "../styles/ChoiceTypeCSS";
 
 const ChoiceType = () => {
   const [choice, setChoice] = useState("");
   const [search, setSearch] = useState("");
   const [result, setResult] = useState();
+  // loading시 로딩 아이콘 처리 state
+  const [isLoading, setLoading] = useState();
 
-  const HOST = `https://dapi.kakao.com/v2/search/${choice}?query=${search}`;
-  const API_KEY = import.meta.env.VITE_REACT_API_KEY;
-
-  const getApiData = async () => {
+  const getApiData = async (choice) => {
+    const HOST = `https://dapi.kakao.com/v2/search/${choice}?query=${search}`;
+    const API_KEY = "736ee0de974dbdf16ba7d5e0586ddf3f";
     // Fetch를 통해 API랑 통신 및 promise 반환
     const data = await fetch(HOST, {
       method: "GET",
@@ -31,16 +41,20 @@ const ChoiceType = () => {
     setResult(json.documents);
   };
 
-  const searchResult = (e) => {
-    e.preventDefault();
+  // category = "web", "blog", "vclicb","image"
+  const changeChoiceResult = (category) => {
+    // 만일 지금 category와 choice state가 다르다면 fetch 한번 더
     if (search === "") {
       return alert("공백은 금지 입니다.");
     }
-    if (choice === "") {
-      setSearch("");
-      return alert("검색 항목을 선택해주셔야 합니다 !");
+
+    if (category !== choice) {
+      console.log("카테고리 다름");
+      setChoice(category);
+      getApiData(category);
+    } else {
+      setChoice(category);
     }
-    getApiData();
   };
 
   return (
@@ -48,26 +62,38 @@ const ChoiceType = () => {
       <MainName>SBDM</MainName>
       <Header>
         <LogoImg src={Logo} />
-        <UserForm onSubmit={(e) => searchResult(e)} id="submit-info">
-          <UserInput onChange={(e) => setSearch(e.target.value)} type="text" placeholder="검색어를 기입해주세요" value={search} />
-        </UserForm>
-        <SubmitBtn type="submit" form="submit-info">
-          검색
-        </SubmitBtn>
+        <UserInput
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="검색어를 기입해주세요"
+          value={search}
+        />
       </Header>
 
       <ChoiceBox>
         {/* 클릭시 CSS 기능 추가 예정 */}
-        <SearchType onClick={() => setChoice("web")} className={choice === "web" ? "active" : ""}>
+        <SearchType
+          onClick={() => changeChoiceResult("web")}
+          className={choice === "web" ? "active" : ""}
+        >
           🌐 웹 문서 🌐
         </SearchType>
-        <SearchType onClick={() => setChoice("blog")} className={choice === "blog" ? "active" : ""}>
+        <SearchType
+          onClick={() => changeChoiceResult("blog")}
+          className={choice === "blog" ? "active" : ""}
+        >
           📚 블로그 📚
         </SearchType>
-        <SearchType onClick={() => setChoice("vclip")} className={choice === "vclip" ? "active" : ""}>
+        <SearchType
+          onClick={() => changeChoiceResult("vclip")}
+          className={choice === "vclip" ? "active" : ""}
+        >
           📽️ 동영상 📽️
         </SearchType>
-        <SearchType onClick={() => setChoice("image")} className={choice === "image" ? "active" : ""}>
+        <SearchType
+          onClick={() => changeChoiceResult("image")}
+          className={choice === "image" ? "active" : ""}
+        >
           🌆 이미지 🌆
         </SearchType>
       </ChoiceBox>
