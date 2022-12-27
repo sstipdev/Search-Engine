d8d3e2bde30aad15bd6d746f50f333e918a386c4
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-12-27 23:32:08 +0900

    소셜 Google 로그인 기능 구현

--- src/components/SNS-Login/GoogleLogin.jsx
+++ src/components/SNS-Login/GoogleLogin.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { GoogleAuthProvider, GithubAuthProvider, signInWithPopup } from "firebase/auth";
-
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../../Auth";
 const GoogleLogin = () => {
+  const onClickGoogleLogin = async (e) => {
+    let provider;
+    const {
+      target: { name },
+    } = e;
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+      await signInWithPopup(auth, provider);
+    }
+  };
   return (
     <div>
-      <button>구글로 로그인</button>
+      <button name="google" onClick={onClickGoogleLogin}>
+        구글로 로그인
+      </button>
     </div>
   );
 };
