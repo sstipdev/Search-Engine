64c62cfafbf1bf76443bf285750c922be6c4ba53
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-12-28 21:33:32 +0900

    Google 로그인 퍼블리싱

--- src/components/SNS-Login/GoogleLogin.jsx
+++ src/components/SNS-Login/GoogleLogin.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../../Auth";
+
+import styles from "../../styles/components/SNS-Login/GoogleLogin.module.css";
+
 const GoogleLogin = () => {
   const onClickGoogleLogin = async (e) => {
     let provider;
@@ -13,9 +16,9 @@ const GoogleLogin = () => {
     }
   };
   return (
-    <div>
-      <button name="google" onClick={onClickGoogleLogin}>
-        구글로 로그인
+    <div className={styles.goole_login_wrap}>
+      <button name="google" onClick={onClickGoogleLogin} className={styles.google_connect}>
+        Google 로그인
       </button>
     </div>
   );
