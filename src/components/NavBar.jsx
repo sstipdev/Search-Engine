ecf53d66440512940680535ae0eb9a6a8069e776
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-12-29 01:03:18 +0900

    헤더메뉴 퍼블리싱 (일부 수정해야함)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,14 @@
 import styles from "../styles/components/NavBar.module.css";
 
+import { useState } from "react";
+
 const NavBar = () => {
-  return <div className={styles.btn}>메뉴</div>;
+  const [nav, setNav] = useState(false);
+  const showNavBarMenu = () => setNav((prev) => !prev);
+  return (
+    <div className={styles.btn} id={nav ? styles.show : styles.hide} onClick={showNavBarMenu}>
+      <div>메뉴</div>
+    </div>
+  );
 };
 export default NavBar;
