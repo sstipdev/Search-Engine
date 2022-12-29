94db15db26f50530e841cefa2c7e1c555d1cbd2a
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-12-29 21:58:31 +0900

    navbar 퍼블리싱, 애니메이션 추가

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,37 @@
-import styles from "../styles/components/NavBar.module.css";
-
 import { useState } from "react";
+
+import { signOut, getAuth } from "firebase/auth";
+
+// CSS
+import styles from "../styles/components/NavBar.module.css";
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
   const showNavBarMenu = () => setNav((prev) => !prev);
+
+  const LogOut = async () => {
+    const auth = getAuth();
+    await signOut(auth);
+  };
+
   return (
     <div className={styles.btn} id={nav ? styles.show : ""}>
       <div className={styles.test}>
         <span onClick={showNavBarMenu}>{!nav && "메뉴"}</span>
       </div>
-      <div id={nav ? styles.showModal : ""}>
+      <div id={nav ? styles.showNavBar : ""}>
         {nav && (
-          <div onClick={showNavBarMenu} className={styles.modalText}>
-            반갑습니다
+          <div className={styles.navbar_info}>
+            <div className={styles.navbar_text}>반갑습니다</div>
+
+            <div className={styles.navbar_footer} onClick={showNavBarMenu}>
+              <button className={styles.navbar_footer_text}>닫기</button>
+            </div>
+            <div>
+              <button onClick={LogOut} className={styles.logout_btn}>
+                로그아웃
+              </button>
+            </div>
           </div>
         )}
       </div>
