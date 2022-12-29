e6a3272a0af1610eaa4336bfc3616ffeea1d6ddb
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-12-30 01:29:26 +0900

    404 page 퍼블리싱

--- src/App.jsx
+++ src/App.jsx
@@ -19,15 +19,15 @@ function App() {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setUserName(auth.currentUser.displayName);
+        navigate("/main");
+
         // 로그인이 되었다면 state 함수 true로 업뎃
         setLogin(true);
         // 로그인이 되었을경우 main 컴포넌트로 이동.
-        navigate("/main");
       } else {
         // 아니면 false로 업뎃
         setLogin(false);
         // 로그인이 안되었을 경우 / 로그인 페이지로 이동
-        navigate("/login");
       }
       setLoading(false);
     });
@@ -44,6 +44,7 @@ function App() {
         </Routes>
       ) : (
         <Routes>
+          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/*" element={<NotPage />} />
         </Routes>
--- src/routes/NotPage.jsx
+++ src/routes/NotPage.jsx
@@ -1,5 +1,16 @@
+import styles from "../styles/routes/NotPage.module.css";
+import { Link } from "react-router-dom";
+
 const NotPage = () => {
-  return <div>404</div>;
+  return (
+    <div className={styles.page_error_wrap}>
+      <div className={styles.page_error_code}>404</div>
+      <div className={styles.page_error_desc}>현재 접속하신 주소는 올바른 주소가 아닙니다.</div>
+      <Link to="/login" className={styles.page_error_link}>
+        홈으로
+      </Link>
+    </div>
+  );
 };
 
 export default NotPage;
