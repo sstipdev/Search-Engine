4ca1aad20e61b82b92920b61998540ada6046fcc
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-12-30 21:44:16 +0900

    로딩 컴포넌트 퍼블리싱 적용

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,14 @@
+import styles from "../styles/components/Loading.module.css";
+
 const Loading = () => {
-  return <div>로딩중임</div>;
+  return (
+    <div className={styles.loading_test}>
+      <div className={styles.loading_container}>
+        <div className={styles.loading}></div>
+        <div id={styles.loading_text}>loading</div>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
